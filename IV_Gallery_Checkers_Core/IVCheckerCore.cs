f41665380f515ee8e41cec179fe1471d394b3417
Ivan Suvorov <avatar2329@gmail.com>
2021-09-26 19:39:08 +0600

    [THS] Add Checkers DLL Project.

---
+++ IV_Gallery_Checkers_Core/IVCheckerCore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IV_Gallery_Checkers_Core
+{
+    public class IVCheckerCore
+    {
+    }
+}

b24a552d4b1bf5a28d8fc172c07ffe84d5e85960
Ivan Suvorov <avatar2329@gmail.com>
2021-09-29 17:27:30 +0600

    [THS] Add IV_Sound_Master Lib

---
+++ IV_Sound_Master/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("IV Sound Master Library")]
+[assembly: AssemblyDescription("That Library Created for using sound in IV Simple Apps.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Thegood Choiseworlds inc 2021.")]
+[assembly: AssemblyProduct("IV_Sound_Master")]
+[assembly: AssemblyCopyright("Copyright THS inc 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("fa4ec357-bfd0-404d-916f-9668400b6956")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

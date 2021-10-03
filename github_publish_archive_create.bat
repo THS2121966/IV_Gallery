88e857aeec810a36064a97f3af2bf36d76be2f49
Ivan Suvorov <avatar2329@gmail.com>
2021-10-03 15:02:50 +0600

    [THS] Create GitHub Release Archive Creator.

---
+++ github_publish_archive_create.bat
@@ -0,0 +1,8 @@
+MD .\IV_Gallery_GIT_Release\IV_Gallery
+copy .\IV_Gallery.exe .\IV_Gallery_GIT_Release\IV_Gallery
+copy .\IV_Gallery_Checkers_Core.dll .\IV_Gallery_GIT_Release\IV_Gallery
+copy .\IV_Sound_Master.dll .\IV_Gallery_GIT_Release\IV_Gallery
+copy .\SharpDX.Desktop.dll .\IV_Gallery_GIT_Release\IV_Gallery
+copy .\SharpDX.dll .\IV_Gallery_GIT_Release\IV_Gallery
+"C:\Program Files\WinRAR\WinRAR.exe" a -ag -m5 ".\IV_Gallery_GIT_Release\IV_Gallery_Release.rar" ".\IV_Gallery_GIT_Release"
+pause

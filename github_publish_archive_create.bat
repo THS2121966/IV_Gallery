63230369cf377bdfad911af9b52582d89ef34f50
Ivan Suvorov <avatar2329@gmail.com>
2021-10-05 00:32:26 +0600

    Update github_publish_archive_create.bat

--- github_publish_archive_create.bat
+++ github_publish_archive_create.bat
@@ -1,4 +1,9 @@
+echo IV_GIT_RELEASE_CREATOR_STARTING...
+echo IV_CREATING_FOLDERS...
 MD .\IV_Gallery_GIT_Release\IV_Gallery
+MD .\IV_Gallery_GIT_Release\IV_Gallery\iv_shaders_cache
+echo IV_CREATE_FOLDERS_COMPLETE!!!
+echo IV_COPING_MAIN_PROGRAMM_AND_INCLUDES...
 copy .\IV_Gallery.exe .\IV_Gallery_GIT_Release\IV_Gallery
 copy .\IV_Gallery_Checkers_Core.dll .\IV_Gallery_GIT_Release\IV_Gallery
 copy .\IV_Sound_Master.dll .\IV_Gallery_GIT_Release\IV_Gallery
@@ -10,5 +15,8 @@ copy .\SharpDX.Mathematics.dll .\IV_Gallery_GIT_Release\IV_Gallery
 copy .\SharpDX.D3DCompiler.dll .\IV_Gallery_GIT_Release\IV_Gallery
 copy .\iv_shaders_cache\gallery_test_pixelShader.hlsl .\IV_Gallery_GIT_Release\IV_Gallery\iv_shaders_cache
 copy .\iv_shaders_cache\gallery_test_vertexShader.hlsl .\IV_Gallery_GIT_Release\IV_Gallery\iv_shaders_cache
+echo IV_COPIED_MAIN_PROGRAMM_AND_INCLUDES!!!
+echo IV_ARCHIVE_MAIN_FOLDER_FOR_RELEASING_THAT...
 "C:\Program Files\WinRAR\WinRAR.exe" a -ag -m5 ".\IV_Gallery_GIT_Release\IV_Gallery_Release.rar" ".\IV_Gallery_GIT_Release"
+echo IV_MAIN_FOLDER_WAS_ARCHIVED!!!
 pause

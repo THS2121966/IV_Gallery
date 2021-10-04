a36217019be273abd1e164b7ffa16f05529045c9
Ivan Suvorov <avatar2329@gmail.com>
2021-10-04 23:50:43 +0600

    [THS] Realise Triangle for test IVD3X Rendering System.

--- IV_Gallery_Main_Menu.cs
+++ IV_Gallery_Main_Menu.cs
@@ -12,6 +12,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using SharpDX;
 using SharpDX.Direct3D;
+using SharpDX.D3DCompiler;
 using SharpDX.DXGI;
 using SharpDX.Windows;
 using D3D11 = SharpDX.Direct3D11;
@@ -286,6 +287,22 @@ namespace IV_Gallery
         private D3D11.DeviceContext IVDXDeviceContext;
         private SwapChain IVswapChain;
         private D3D11.RenderTargetView IVrenderTargetView;
+        private Viewport IV3DXviewport;
+
+        // Shaders
+        private D3D11.VertexShader IV_G_Test_VertexShader;
+        private D3D11.PixelShader IV_G_Test_PixelShader;
+        private ShaderSignature IVEngine_inputSignature;
+        private D3D11.InputLayout IVEngine_inputLayout;
+
+        private D3D11.InputElement[] IVEngine_inputElements = new D3D11.InputElement[]
+        {
+            new D3D11.InputElement("POSITION", 0, Format.R32G32B32_Float, 0)
+        };
+
+        // Triangle vertices
+        private Vector3[] IVvertices = new Vector3[] { new Vector3(-0.5f, 0.5f, 0.0f), new Vector3(0.5f, 0.5f, 0.0f), new Vector3(0.0f, -0.5f, 0.0f) };
+        private D3D11.Buffer IVtriangleVertexBuffer;
 
         public DXCoreTest()
         {
@@ -327,12 +344,12 @@ namespace IV_Gallery
 
         private void IV3DXInitializeDeviceResources()
         {
-            ModeDescription ivdx_backBufferDesc = new ModeDescription(Width, Height, new Rational(60, 1), Format.R8G8B8A8_UNorm);
+            ModeDescription IVDXbackBufferDesc = new ModeDescription(Width, Height, new Rational(60, 1), Format.R8G8B8A8_UNorm);
 
             // Descriptor for the swap chain
-            SwapChainDescription ivdx_swapChainDesc = new SwapChainDescription()
+            SwapChainDescription IVDXswapChainDesc = new SwapChainDescription()
             {
-                ModeDescription = ivdx_backBufferDesc,
+                ModeDescription = IVDXbackBufferDesc,
                 SampleDescription = new SampleDescription(1, 0),
                 Usage = Usage.RenderTargetOutput,
                 BufferCount = 1,
@@ -341,14 +358,53 @@ namespace IV_Gallery
             };
 
             // Create device and swap chain
-            D3D11.Device.CreateWithSwapChain(DriverType.Hardware, D3D11.DeviceCreationFlags.None, ivdx_swapChainDesc, out IVDXDevice, out IVswapChain);
+            D3D11.Device.CreateWithSwapChain(DriverType.Hardware, D3D11.DeviceCreationFlags.None, IVDXswapChainDesc, out IVDXDevice, out IVswapChain);
             IVDXDeviceContext = IVDXDevice.ImmediateContext;
+
+            IV3DXviewport = new Viewport(0, 0, Width, Height);
+            IVDXDeviceContext.Rasterizer.SetViewport(IV3DXviewport);
 
             // Create render target view for back buffer
             using (D3D11.Texture2D backBuffer = IVswapChain.GetBackBuffer<D3D11.Texture2D>(0))
             {
                 IVrenderTargetView = new D3D11.RenderTargetView(IVDXDevice, backBuffer);
             }
+        }
+
+        private void IV3DXInitializeShaders()
+        {
+            // Compile the vertex shader code
+            using (var IVEngineVertexShaderByteCode = ShaderBytecode.CompileFromFile("iv_shaders_cache/gallery_test_vertexShader.hlsl", "main", "vs_4_0", ShaderFlags.Debug))
+            {
+                // Read input signature from shader code
+                IVEngine_inputSignature = ShaderSignature.GetInputSignature(IVEngineVertexShaderByteCode);
+
+                IV_G_Test_VertexShader = new D3D11.VertexShader(IVDXDevice, IVEngineVertexShaderByteCode);
+            }
+
+            // Compile the pixel shader code
+            using (var IVEnginePixelShaderByteCode = ShaderBytecode.CompileFromFile("iv_shaders_cache/gallery_test_pixelShader.hlsl", "main", "ps_4_0", ShaderFlags.Debug))
+            {
+                IV_G_Test_PixelShader = new D3D11.PixelShader(IVDXDevice, IVEnginePixelShaderByteCode);
+            }
+
+            // Set as current vertex and pixel shaders
+            IVDXDeviceContext.VertexShader.Set(IV_G_Test_VertexShader);
+            IVDXDeviceContext.PixelShader.Set(IV_G_Test_PixelShader);
+
+            IVDXDeviceContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleList;
+
+            // Create the input layout from the input signature and the input elements
+            IVEngine_inputLayout = new D3D11.InputLayout(IVDXDevice, IVEngine_inputSignature, IVEngine_inputElements);
+
+            // Set input layout to use
+            IVDXDeviceContext.InputAssembler.InputLayout = IVEngine_inputLayout;
+        }
+
+        private void IV3DXInitializeTriangle()
+        {
+            // Create a vertex buffer, and use our array with vertices as data
+            IVtriangleVertexBuffer = D3D11.Buffer.Create<Vector3>(IVDXDevice, D3D11.BindFlags.VertexBuffer, IVvertices);
         }
 
         private void IV_INIT_D3X_Window()
@@ -363,6 +419,8 @@ namespace IV_Gallery
             DXWnd.PerformLayout();
 
             IV3DXInitializeDeviceResources();
+            IV3DXInitializeShaders();
+            IV3DXInitializeTriangle();
         }
 
         private void IV_RUN_D3X_Window_Simple()
@@ -380,6 +438,12 @@ namespace IV_Gallery
 
             // Clear the screen
             IVDXDeviceContext.ClearRenderTargetView(IVrenderTargetView, new SharpDX.Color(32, 103, 178));
+
+            // Set vertex buffer
+            IVDXDeviceContext.InputAssembler.SetVertexBuffers(0, new D3D11.VertexBufferBinding(IVtriangleVertexBuffer, Utilities.SizeOf<Vector3>(), 0));
+
+            // Draw the triangle
+            IVDXDeviceContext.Draw(IVvertices.Count(), 0);
 
             // Swap front and back buffer
             IVswapChain.Present(1, PresentFlags.None);

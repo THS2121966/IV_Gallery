356cf7d059f0cb1280f5007ff9db70a743f6a979
Ivan Suvorov <avatar2329@gmail.com>
2021-10-04 21:33:15 +0600

    Update github_publish_archive_create.bat

--- github_publish_archive_create.bat
+++ github_publish_archive_create.bat
@@ -4,5 +4,7 @@ copy .\IV_Gallery_Checkers_Core.dll .\IV_Gallery_GIT_Release\IV_Gallery
 copy .\IV_Sound_Master.dll .\IV_Gallery_GIT_Release\IV_Gallery
 copy .\SharpDX.Desktop.dll .\IV_Gallery_GIT_Release\IV_Gallery
 copy .\SharpDX.dll .\IV_Gallery_GIT_Release\IV_Gallery
+copy .\SharpDX.Direct3D11.dll .\IV_Gallery_GIT_Release\IV_Gallery
+copy .\SharpDX.DXGI.dll .\IV_Gallery_GIT_Release\IV_Gallery
 "C:\Program Files\WinRAR\WinRAR.exe" a -ag -m5 ".\IV_Gallery_GIT_Release\IV_Gallery_Release.rar" ".\IV_Gallery_GIT_Release"
 pause
